7f6a1bd76ee525e6ee99da607803c9d5483448bc
John Iverson Santos <153043111+JohnIversonSantos@users.noreply.github.com>
2025-08-19 01:48:16 +0000

    taskSheet-1-1-1

---
+++ HelloWorldApp.java
@@ -0,0 +1,17 @@
+/** 
+ * The HelloWorldApp class implements a simple Java application 
+ * that prints a message to the standard output (console).
+ * 
+ * Original issues fixed:
+ * 1. Changed "clas" > "public class" (correct keyword spelling).
+ * 2. Corrected the class name to "HelloWorldApp" with proper capitalization.
+ * 3. Fixed "System.out.rintln" > "System.out.println" (typo in method name).
+ * 4. Closed the string properly with quotation marks.
+ * 5. Changed the printed message from "Hello World!" to "Kamusta Mundo!".
+ */
+public class HelloWorldApp { 
+    public static void main(String[] args) {  
+        // Print "Kamusta Mundo!"
+        System.out.println("Kamusta Mundo!"); 
+    }
+}
